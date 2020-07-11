de246b021ec1510433b47ce39078bf9a0e851442
Alexander Spivak <po4ta.polevaya@gmail.com>
2020-07-11 18:07:49 +0300

    defining the field of view

--- raycast.js
+++ raycast.js
@@ -5,11 +5,16 @@ const MAP_NUM_COLS = 15;
 const WINDOW_WIDTH = MAP_NUM_COLS * TILE_SIZE;
 const WINDOW_HEIGHT = MAP_NUM_ROWS * TILE_SIZE;
 
+const FOV_ANGLE = 60 * (Math.PI / 180);
+
+const WALL_STRIP_WIDTH = 30;
+const NUM_RAYS = WINDOW_WIDTH / WALL_STRIP_WIDTH;
+
 class Map {
 	constructor() {
 		        this.grid = [
 					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-					[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+					[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
 					[1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1],
 					[1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1],
 					[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1],
@@ -20,6 +25,15 @@ class Map {
 					[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
 					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 				];
+	}
+	hasWallAt(x, y) {
+		if (x < 0 || x > WINDOW_WIDTH || y < 0 || y > WINDOW_HEIGHT)
+		{
+			return (true);
+		}
+		let mapGridIndexX = Math.floor(x / TILE_SIZE);
+		let mapGridIndexY = Math.floor(y / TILE_SIZE);
+		return (this.grid[mapGridIndexY][mapGridIndexX] != 0);
 	}
 	render() {
 		for (let i = 0; i < MAP_NUM_ROWS; i++) {
@@ -50,29 +64,45 @@ class Player {
 		this.rotationAngle += this.turnDirection * this.rotationSpeed;
 		
 		let moveStep = this.walkDirection * this.moveSpeed;
-		let newX = this.x + moveStep * Math.cos(this.rotationAngle); 
-		let newY = this.y + moveStep * Math.sin(this.rotationAngle); 
-		if (grid.grid[Math.floor(newY / 32)][Math.floor(newX / 32)] == 0) {
-			this.x = newX;
-			this.y = newY;
+		let newPlayerX = this.x + moveStep * Math.cos(this.rotationAngle); 
+		let newPlayerY = this.y + moveStep * Math.sin(this.rotationAngle); 
+		if (!grid.hasWallAt(newPlayerX, newPlayerY)) {
+			this.x = newPlayerX;
+			this.y = newPlayerY;
 		}
 	}
 	render() {
 		noStroke();
 		fill("red");
-		circle(this.x, this.y, this.radius);
+		circle(this.x, this.y, this.radius * 2);
 		stroke("red");
 		line(
-			this.x, 
-			this.y, 
+			this.x,
+			this.y,
 			this.x + Math.cos(this.rotationAngle) * 30,
-			this.y + Math.sin(this.rotationAngle) * 30,
+			this.y + Math.sin(this.rotationAngle) * 30
 		);
 	}
 }
 
+class Ray {
+	constructor(rayAngle) {
+		this.rayAngle = rayAngle;
+	}
+	render() {
+		stroke("rgba(255, 0, 0, 0.3)");
+		line(
+			player.x, 
+			player.y,
+			player.x + Math.cos(this.rayAngle) * 30,
+			player.y + Math.sin(this.rayAngle) * 30
+		);
+	}
+
+}
 var grid = new Map();
 var player = new Player();
+var rays = [];
 
 function keyPressed() {
 	if (keyCode == UP_ARROW) {
@@ -98,18 +128,42 @@ function keyReleased() {
 	}
 }
 
+function castAllRays() {
+	let columnId = 0;
+
+	//start first ray subtracting half of the FOV
+	let rayAngle = player.rotationAngle - (FOV_ANGLE / 2);
+
+	rays = [];
+
+	//loop all columns casing the rays
+	for (let i = 0; i < NUM_RAYS; i++)
+	{
+		let ray = new Ray(rayAngle);
+		// ray.cast();
+		rays.push(ray);
+
+		rayAngle += FOV_ANGLE / NUM_RAYS;
+	
+		columnId++;
+	}
+}
 function setup() {
 	createCanvas(WINDOW_WIDTH, WINDOW_HEIGHT);
 }
 
 function update () {
 	player.update();
+	castAllRays();
 }
 
 function draw() {
 	update();
 
 	grid.render();
+	for (ray of rays) {
+		ray.render();
+	}
 	player.render();
 }
 
